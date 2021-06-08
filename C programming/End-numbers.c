a67726f1ddd8b9f04f4802568fd67d7e1ac523b5
Mary Golub <golub202me@gmail.com>
2021-06-08 11:30:44 +0300

    Цифры в конец
    Строка состоит всего из одного какого-то слова. В слово входят буквы и цифры. Ваша задача переместить все цифры в конец строки.
    Порядок цифр и букв необходимо сохранить
    ghj77kl8dd ==> ghjkldd778
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    Формат ввода:
    Исходная строка, содержащая латинские буквы и цифры, длина строки от 1 до 10000 символов
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    Формат вывода:
    Результирующая строка

---
+++ C programming/End-numbers.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 10000
+
+int main()
+{
+    char str[SIZE] = { 0 };
+	fgets(str, SIZE, stdin);
+    int len = strlen(str) - 1;
+	str[len] = '\0';
+	char str2[SIZE] = { 0 };
+	str2[len] = '\0';
+	int k = 0;
+	for (int i = 0; i < len; ++i)
+	{
+	    if(str[i]>='A'){str2[k]=str[i]; k+=1;}
+	}
+	for (int i = 0; i < len; ++i)
+	{
+	    if(str[i]<'A'){str2[k]=str[i]; k+=1;}
+	}
+	printf("%s\n", str2);
+}
